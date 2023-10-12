29f9acf67c23bdd5d912de6e6b395e19ef20c314
pepe <dani_rc_22@hotmail.com>
2023-10-12 20:38:17 -0300

    creacion de la estructura del proyecto

---
+++ ModeloPrediccionLluvia/main.py
@@ -0,0 +1,21 @@
+class ModeloPrediccionLluvia:
+    def __init__(self, archivo_datos):
+        pass
+
+    def limpiar_datos(self):
+        pass
+
+    def preprocesar_datos(self):
+        pass
+
+    def entrenar_regresión_lineal(self):
+        pass
+
+    def entrenar_regresión_regularizada(self, tipo_regularización):
+        pass
+
+    def evaluar_modelo(self, modelo, X_test, y_test, y_pred):
+        pass
+
+    def ejecutar_experimento(self):
+        pass
