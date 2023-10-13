ae7bd99a4b49957fd911242c7232ddf4eb232975
pepe <dani_rc_22@hotmail.com>
2023-10-13 00:06:52 -0300

    avances en limpieza de datos

--- ModeloPrediccionLluvia/main.py
+++ ModeloPrediccionLluvia/main.py
@@ -1,8 +1,14 @@
+import numpy as np
 import pandas as pd
+from matplotlib.widgets import Lasso
 from scipy.stats import stats
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.linear_model import ElasticNet, Ridge, LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
 
 class ModeloPrediccionLluvia:
     def __init__(self, file):
@@ -12,15 +18,13 @@ class ModeloPrediccionLluvia:
         ciudades = ['Sydney', 'SydneyAirport', 'Canberra', 'Melbourne', 'MelbourneAirport']
         datos_filtrados = self.data[self.data['Location'].isin(ciudades)]
         # datos_filtrados.loc[:, 'Date'] = pd.to_datetime(datos_filtrados['Date'])
-        datos_filtrados.loc[datos_filtrados['Location'].duplicated(), 'MinTemp'] = datos_filtrados['MinTemp'].ffill()
         datos_filtrados.loc[:,  'MinTemp'] = datos_filtrados.groupby('Location')['MinTemp'].ffill()
-        datos_filtrados.loc[datos_filtrados['Location'].duplicated(), 'MaxTemp'] = datos_filtrados['MaxTemp'].ffill()
         datos_filtrados.loc[:, 'MaxTemp'] = datos_filtrados.groupby('Location')['MaxTemp'].ffill()
         datos_filtrados.loc[:, 'Rainfall'] = datos_filtrados['Rainfall'].fillna(0)
         datos_filtrados.loc[:, 'Temp9am'] = datos_filtrados['Temp9am'].ffill()
         datos_filtrados.loc[:, 'Temp3pm'] = datos_filtrados['Temp9am'].ffill()
         datos_filtrados.loc[:, 'Humidity3pm'] = datos_filtrados['Humidity3pm'].ffill()
-
+        datos_filtrados.loc[:, 'Cloud3pm'] = datos_filtrados['Cloud3pm'].ffill()
         umbral_z = 3
         columnas_numericas = datos_filtrados.select_dtypes(include='number').columns
         for columna in columnas_numericas:
@@ -74,20 +78,113 @@ class ModeloPrediccionLluvia:
         sns.heatmap(matriz_correlacion, annot=True, cmap='coolwarm', linewidths=0.5)
         plt.title('Matriz de Correlación (sin Date)')
         plt.show()
+        conteo_clases = self.data_clean['RainTomorrow'].value_counts()
+
+        # Calcula el equilibrio
+        porcentaje_clase_positiva = (conteo_clases['Yes'] / len(self.data_clean)) * 100
+        porcentaje_clase_negativa = (conteo_clases['No'] / len(self.data_clean)) * 100
+
+        print(f'Porcentaje de clase "Yes" (Lloverá): {porcentaje_clase_positiva:.2f}%')
+        print(f'Porcentaje de clase "No" (No lloverá): {porcentaje_clase_negativa:.2f}%')
 
     def preprocesar_datos(self):
         pass
 
     def entrenar_regresion_lineal(self):
-        pass
+        # Selecciona las columnas de características y la variable objetivo
+        columnas_caracteristicas = ['Rainfall', 'Humidity3pm',  'Cloud3pm']
+        variable_objetivo = 'RainfallTomorrow'
+        # Divide los datos en conjuntos de entrenamiento y prueba
+        X = self.data_clean[columnas_caracteristicas]
+        y = self.data_clean[variable_objetivo]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        # Crea un modelo de regresión lineal
+        modelo = LinearRegression()
+        # Entrena el modelo
+        modelo.fit(X_train, y_train)
+        # Realiza predicciones en el conjunto de prueba
+        y_pred = modelo.predict(X_test)
+        return y_test, y_pred
 
-    def entrenar_regresion_regularizada(self, tipo_regularización):
-        pass
+    def entrenar_regresion_regularizada(self, tipo_regularizacion, alpha=1.0):
+        # Selecciona las columnas de características y la variable objetivo
+        columnas_caracteristicas = ['MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine', 'WindGustSpeed',
+                                    'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am', 'Humidity3pm', 'Pressure9am',
+                                    'Pressure3pm', 'Cloud9am', 'Cloud3pm', 'Temp9am', 'Temp3pm']
+        variable_objetivo = 'RainfallTomorrow'
 
-    def evaluar_modelo(self, modelo, X_test, y_test, y_pred):
-        pass
+        # Divide los datos en conjuntos de entrenamiento y prueba
+        X = self.data_clean[columnas_caracteristicas]
+        y = self.data_clean[variable_objetivo]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        if tipo_regularizacion == 'Lasso':
+            # Crea un modelo de regresión Lasso
+            modelo = Lasso(alpha=alpha)
+        elif tipo_regularizacion == 'Ridge':
+            # Crea un modelo de regresión Ridge
+            modelo = Ridge(alpha=alpha)
+        elif tipo_regularizacion == 'ElasticNet':
+            # Crea un modelo de regresión Elastic Net
+            modelo = ElasticNet(alpha=alpha, l1_ratio=0.5)
+        else:
+            raise ValueError("Tipo de regularización no válido")
+
+        # Entrena el modelo
+        modelo.fit(X_train, y_train)
+
+        # Realiza predicciones en el conjunto de prueba
+        y_pred = modelo.predict(X_test)
+
+        return y_test, y_pred
+
+    def evaluar_modelo(self, y_true, y_pred):
+        # Calcula métricas de rendimiento
+        mse = mean_squared_error(y_true, y_pred)
+        r2 = r2_score(y_true, y_pred)
+        rmse = np.sqrt(mse)
+        mae = np.mean(np.abs(y_true - y_pred))
+        mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+        return {
+            "MSE": mse,
+            "R2": r2,
+            "RMSE": rmse,
+            "MAE": mae,
+            "MAPE": mape
+        }
 
     def ejecutar_experimento(self):
         self.limpiar_datos()
+
+        # Visualiza los datos
         self.visualizar_datos()
-        pass
+
+        # Preprocesa los datos si es necesario
+        self.preprocesar_datos()
+
+        # Entrenamiento y evaluación de modelos
+        # Entrenar y evaluar el modelo de regresión lineal
+        y_true, y_pred = self.entrenar_regresion_lineal()
+        metricas = self.evaluar_modelo(y_true, y_pred)
+        print("Métricas del modelo de regresión lineal:")
+        print(metricas)
+
+        # Entrenar y evaluar el modelo de regresión regularizada (Lasso)
+        y_true, y_pred = self.entrenar_regresion_regularizada("Lasso", alpha=0.01)
+        metricas = self.evaluar_modelo(y_true, y_pred)
+        print("Métricas del modelo de regresión Lasso:")
+        print(metricas)
+
+        # Entrenar y evaluar el modelo de regresión regularizada (Ridge)
+        y_true, y_pred = self.entrenar_regresion_regularizada("Ridge", alpha=0.01)
+        metricas = self.evaluar_modelo(y_true, y_pred)
+        print("Métricas del modelo de regresión Ridge:")
+        print(metricas)
+
+        # Entrenar y evaluar el modelo de regresión regularizada (ElasticNet)
+        y_true, y_pred = self.entrenar_regresion_regularizada("ElasticNet", alpha=0.01)
+        metricas = self.evaluar_modelo(y_true, y_pred)
+        print("Métricas del modelo de regresión ElasticNet:")
+        print(metricas)
+
