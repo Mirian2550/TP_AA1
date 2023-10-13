369a18b59dd2ba07d862102974a22b2044fa6119
pepe <dani_rc_22@hotmail.com>
2023-10-12 21:05:04 -0300

    creacion de la estructura del proyecto

--- ModeloPrediccionLluvia/main.py
+++ ModeloPrediccionLluvia/main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 class ModeloPrediccionLluvia:
     def __init__(self, file):
-        self.data: pd.read_csv(file)
-        self.data_clean: None
+        self.data = pd.read_csv(file)
+        self.data_clean = None
     def limpiar_datos(self):
         ciudades = ['Sydney', 'SydneyAirport', 'Canberra', 'Melbourne', 'MelbourneAirport']
-        datos_filtrados = self.data[self.data['Location'].isin(self.ciudades)]
+        datos_filtrados = self.data[self.data['Location'].isin(ciudades)]
         print(datos_filtrados)
 
     def visualizar_datos(self):
