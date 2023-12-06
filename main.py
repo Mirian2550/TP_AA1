b0720ea1b333c58b4c5de46aa092da14a0bec1b4
pepeargentoo <dani_rc_22@hotmail.com>
2023-12-06 19:30:57 -0300

    punto 5

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from ModeloPrediccionLluvia.main import ModeloPrediccionLluvia, evaluar_regresion_logistica
+from ModeloBase.regression import RegresionModelBase
+from ModeloBase.clasification import ClasificationModelBase
+
 archivo_datos = 'data/weatherAUS.csv'
 modelo = ModeloPrediccionLluvia(archivo_datos)
 resultados = modelo.regresiones_lineales()
 y_test, y_pred = modelo.regresion_logistica()
 evaluar_regresion_logistica(y_test=y_test, y_pred=y_pred)
+# modelos base de regresion
+# this is model base of regression
+
+print('Modelo Base de Regresion')
+modelo_regresion = RegresionModelBase(modelo.data_clean)
+modelo_regresion.regresion()
+print('Modelo de clasificacion ')
+modelo_clasificacion = ClasificationModelBase(modelo.data_clean)
+modelo_clasificacion.clasificacion()
